b13cedf94fb0fc2803a028c56ceff87f85a21a6a
kodemapper <saranganilgade@gmail.com>
2025-08-27 11:29:45 +0530

    Pattern print - p4

---
+++ Strings/src/com/patternPrint/Ptrn_4.java
@@ -0,0 +1,25 @@
+package com.patternPrint;
+
+public class Ptrn_4 {
+
+    static void print(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        print(5);
+
+        /*
+            1
+            12
+            123
+            1234
+            12345
+         */
+    }
+}
