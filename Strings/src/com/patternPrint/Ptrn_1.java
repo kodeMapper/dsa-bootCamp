e0c3b1c0a62979154216fda6a3655b998ffc3bcb
kodemapper <saranganilgade@gmail.com>
2025-08-27 11:12:24 +0530

    Pattern printing - p1

---
+++ Strings/src/com/patternPrint/Ptrn_1.java
@@ -0,0 +1,25 @@
+package com.patternPrint;
+
+public class Ptrn_1 {
+
+    static void print(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        print(5);
+
+        /*
+            *****
+            *****
+            *****
+            *****
+            *****
+         */
+    }
+}
