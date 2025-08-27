4e69da2363108b509d9658e8d2e944408cf4495a
kodemapper <saranganilgade@gmail.com>
2025-08-27 12:45:46 +0530

    Pattern priting - p30

---
+++ Strings/src/com/patternPrint/Ptrn_30.java
@@ -0,0 +1,26 @@
+package com.patternPrint;
+
+public class Ptrn_30 {
+
+    static void print(int n) {
+        for (int row = 1; row <= n; row++) {
+
+            for (int space = 0; space < n-row; space++) {
+                System.out.print("  ");
+            }
+
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        print(5);
+    }
+}
