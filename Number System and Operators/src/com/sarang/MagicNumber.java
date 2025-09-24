2dfbee4231ed523fdcc769c80a263e109d95bd3a
kodemapper <saranganilgade@gmail.com>
2025-09-24 20:34:10 +0530

    magic number approach added

--- Number System and Operators/src/com/sarang/MagicNumber.java
+++ Number System and Operators/src/com/sarang/MagicNumber.java
@@ -1,5 +1,14 @@
 package com.sarang;
+/*
+    Q. Find nth magic number
+    1st magic number --> write 1 in binary and multiply it with powers of 5
+    0    0      1
+    *    *      *
+    5^3 + 5^2 + 5^1 = 5
 
+    approach -
+    convert the number in binary --> we need the last bit --> hence & with 1 will give us the last digit --> multiply it with 5^i --> right shift --> repeat the process
+ */
 public class MagicNumber {
 
     public static void main(String[] args) {
