c0897463e5f56b3dd6c8bfd8748697e4a0a5b50c
kodemapper <saranganilgade@gmail.com>
2025-09-26 17:10:59 +0530

    Number of Digits in number N with base b

---
+++ Number System and Operators/src/com/sarang/noOfDigits.java
@@ -0,0 +1,16 @@
+package com.sarang;
+
+public class noOfDigits {
+
+    public static void main(String[] args) {
+        int b = 2;
+        int num = 10;
+        System.out.println("Number of Digits in 10 in base 2: " + countDigits(b, num));
+    }
+
+    static int countDigits(int b, int num) {
+        int count = 0;
+        count = (int)(Math.log(num) / Math.log(b)) + 1;
+        return count;
+    }
+}
