c8c7104198fc8647e599977646275939a89ed541
kodemapper <saranganilgade@gmail.com>
2025-10-01 17:34:22 +0530

    Newton's method for calculating the square root

---
+++ Number System and Operators/src/com/sarang/NewtonSqrt.java
@@ -0,0 +1,23 @@
+package com.sarang;
+
+public class NewtonSqrt {
+
+    static double sqrt(double n) {
+        double x = n;
+        double root;
+        while(true) {
+            root = 0.5 * (x + (n / x));
+
+            if (Math.abs(root - x) < 0.2) {
+                break;
+            }
+            x = root;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sqrt(40));;
+    }
+}
