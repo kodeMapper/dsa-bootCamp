a49b27438e88ddc1bf84093d1c1da6cd8c3f4d7a
kodemapper <saranganilgade@gmail.com>
2026-01-07 23:51:24 +0530

    Questions started related to hashmap and hashset

--- HashMap_HashSet_HashTable/src/Questions/MajorityElement.java
+++ HashMap_HashSet_HashTable/src/Questions/MajorityElement.java
@@ -1,4 +1,23 @@
 package Questions;
 
+import java.util.*;
+
 public class MajorityElement {
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] arr = {1,3,2,5,1,3,1,5,1};
+        for (int elem : arr) {
+            if (map.containsKey(elem)) {
+                map.put(elem, map.get(elem) + 1);
+            } else {
+                map.put(elem, 1);
+            }
+        }
+
+        for(int key: map.keySet()) {
+            if(map.get(key) > arr.length/3) {
+                System.out.println(key);
+            }
+        }
+    }
 }
--- HashMap_HashSet_HashTable/src/Questions/UnionOf2Arrays.java
+++ HashMap_HashSet_HashTable/src/Questions/UnionOf2Arrays.java
@@ -1,4 +1,20 @@
 package Questions;
 
+import java.util.HashSet;
+
 public class UnionOf2Arrays {
+    public static void main(String[] args) {
+        int[] arr1 = {7, 3, 9};
+        int[] arr2 = {6,3,9,2,9,4};
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int i=0; i<arr1.length; i++) {
+            set.add(arr1[i]);
+        }
+        for (int i=0; i<arr2.length; i++) {
+            set.add(arr2[i]);
+        }
+
+        System.out.println(set.size());
+    }
 }
