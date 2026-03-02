9052fdfe20ec44bf5b7afb2853832e82b2723d42
kodemapper <saranganilgade@gmail.com>
2026-03-03 02:10:20 +0530

    other syntax - cleaner

--- Practice-Mix/src/leetcode/MajorityElement.java
+++ Practice-Mix/src/leetcode/MajorityElement.java
@@ -31,6 +31,28 @@ public class MajorityElement {
         return 0;
     }
 
+    /*
+
+        public static void main(String[] args) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] arr = {1,3,2,5,1,3,1,5,1};
+        for (int elem : arr) {
+            if (map.containsKey(elem)) {
+                map.put(elem, map.get(elem) + 1);
+            } else {
+                map.put(elem, 1);
+            }
+        }
+
+        for(int key: map.keySet()) {
+            if(map.get(key) > arr.length/3) {
+                System.out.println(key);
+            }
+        }
+    }
+
+     */
+
     public static void main(String[] args) {
         int[] nums = {3,2,3};
         System.out.println(majorityElement(nums));
