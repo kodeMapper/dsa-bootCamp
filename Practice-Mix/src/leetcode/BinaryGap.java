c88aacca383b9943725ad11c517846c370f723a2
kodemapper <saranganilgade@gmail.com>
2026-03-02 19:17:05 +0530

    sliding window practice

---
+++ Practice-Mix/src/leetcode/BinaryGap.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+public class BinaryGap {
+    public static int binaryGap(int n) {
+        String binary = Integer.toBinaryString(n);
+        int left = 0;
+        int right = 0;
+        int maxDist = 0;
+        boolean isFirst = false;
+
+        for(right=0; right<binary.length(); right++) {
+            if(binary.charAt(right)==1 && !isFirst) {
+                isFirst = true;
+            }
+
+            else if(binary.charAt(right)==1 && isFirst) {
+                left = right;
+                maxDist = Math.max(maxDist, right-left+1);
+            }
+        }
+
+        return maxDist;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binaryGap(22));;
+    }
+}
