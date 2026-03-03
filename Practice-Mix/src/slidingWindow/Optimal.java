ca4d6072f05f3e05d5d29e699e8b6a4e0332ae07
kodemapper <saranganilgade@gmail.com>
2026-03-03 15:32:56 +0530

    Optimal Appraoch

---
+++ Practice-Mix/src/slidingWindow/Optimal.java
@@ -0,0 +1,25 @@
+package slidingWindow;
+
+public class Optimal {
+
+    public static void main(String[] args) {
+        int l=0, r=0, sum=0, maxL=0, k=14;
+        int[] arr = {2,5,1,7,10};
+        while(r < arr.length) {
+            sum = sum + arr[r];
+
+            if(sum > k) {
+                sum = sum - arr[l];
+                l++;
+            }
+
+            if(sum <= k) {
+                maxL = Math.max(maxL, r-l+1);
+            }
+
+            r++;
+        }
+
+        System.out.println(maxL);
+    }
+}
