8bbe1caaba604d37f61f4e512b70e2dd218f2eb6
kodemapper <saranganilgade@gmail.com>
2026-03-03 15:32:15 +0530

    Sliding window - better

--- Practice-Mix/src/slidingWindow/betterApproach.java
+++ Practice-Mix/src/slidingWindow/betterApproach.java
@@ -1,4 +1,23 @@
 package slidingWindow;
 
 public class betterApproach {
+    public static void main(String[] args) {
+        int l=0, r=0, sum=0, maxL=0, k=14;
+        int[] arr = {2,5,1,7,10};
+        while(r < arr.length) {
+            sum = sum + arr[r];
+            while(sum > k) {
+                sum = sum - arr[l];
+                l++;
+            }
+
+            if(sum <= k) {
+                maxL = Math.max(maxL, r-l+1);
+            }
+
+            r++;
+        }
+
+        System.out.println(maxL);
+    }
 }
