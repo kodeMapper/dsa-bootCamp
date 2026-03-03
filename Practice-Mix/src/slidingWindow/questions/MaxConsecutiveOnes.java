61d4205296999ab8f5566f1884537b7c1aff889c
kodemapper <saranganilgade@gmail.com>
2026-03-03 19:08:38 +0530

    Optimal solution for Max Consecutive Ones

--- Practice-Mix/src/slidingWindow/questions/MaxConsecutiveOnes.java
+++ Practice-Mix/src/slidingWindow/questions/MaxConsecutiveOnes.java
@@ -27,4 +27,23 @@ public class MaxConsecutiveOnes {
     }
 
      */
+
+    // Optimal -
+
+    public int longestOnes(int[] nums, int k) {
+        int l = 0, zeros = 0, maxLen = 0;
+
+        for (int r = 0; r < nums.length; r++) {
+            if (nums[r] == 0) zeros++;
+
+            if (zeros > k) {
+                if (nums[l] == 0) zeros--;
+                l++;
+            }
+
+            maxLen = Math.max(maxLen, r - l + 1);
+        }
+
+        return maxLen;
+    }
 }
