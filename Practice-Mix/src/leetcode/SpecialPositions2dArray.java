ca103b10d0ba0691ceadd7f383835f5a188f6115
kodemapper <saranganilgade@gmail.com>
2026-03-04 23:35:27 +0530

    leetcode daily question

---
+++ Practice-Mix/src/leetcode/SpecialPositions2dArray.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+public class SpecialPositions2dArray {
+    public static int numSpecial(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[] rowSum = new int[m];
+        int[] colSum = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                rowSum[i] += mat[i][j];
+                colSum[j] += mat[i][j];
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 && rowSum[i] == 1 && colSum[j] == 1) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[][] mat = {{1,0,0},{0,0,1},{1,0,0}};
+        System.out.println(numSpecial(mat));
+    }
+}
