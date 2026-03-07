8fd4696e9ece08275f15fc9e3b814a0963af629e
kodemapper <saranganilgade@gmail.com>
2026-03-07 22:57:58 +0530

    New Leetcode problem

---
+++ Practice-Mix/src/slidingWindow/questions/NumberofSubstringsContainingAllThreeCharacters.java
@@ -0,0 +1,21 @@
+package slidingWindow.questions;
+
+public class NumberofSubstringsContainingAllThreeCharacters {
+    public static void main(String[] args) {
+        String s = "abcabc";
+        int n = s.length();
+        int[] count = new int[3];
+        int left = 0, result = 0;
+
+        for (int right = 0; right < n; right++) {
+            count[s.charAt(right) - 'a']++;
+
+            while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
+                result += n - right;
+                count[s.charAt(left) - 'a']--;
+                left++;
+            }
+        }
+        System.out.println(result);
+    }
+}
