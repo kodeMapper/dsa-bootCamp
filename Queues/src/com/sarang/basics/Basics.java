2a426a16b1f8217915f4a79a060b6e7e4b3bb266
kodeMapper <saranganilgade@gmail.com>
2026-08-20 18:29:02 +0530

    queue basics - initialize, remove, poll, element, peek

--- Queues/src/com/sarang/basics/Basics.java
+++ Queues/src/com/sarang/basics/Basics.java
@@ -1,5 +1,20 @@
 package com.sarang.basics;
 
-public class Basics {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Basics {
+    static void main() {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+        q.add(50);
+
+        System.out.println(q);
+        int ans = q.element(); // try remove, poll, peek, element
+        System.out.println(q);
+        System.out.println("ans: " + ans);
+    }
 }
