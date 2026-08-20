934fbe8fb97e6c26822fbaed4d985afa9533582e
kodeMapper <saranganilgade@gmail.com>
2026-08-20 18:38:50 +0530

    display function

--- Queues/src/com/sarang/basics/Basics.java
+++ Queues/src/com/sarang/basics/Basics.java
@@ -4,6 +4,19 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Basics {
+    static void display(Queue<Integer> q) {
+        Queue<Integer> temp = new LinkedList<>();
+        while(q.peek() != null) {
+            int val = q.poll();
+            System.out.print(val + " ");
+            temp.add(val);
+        }
+        System.out.println();
+        while(temp.peek() != null) {
+            int val = temp.poll();
+            q.add(val);
+        }
+    }
     static void main() {
         Queue<Integer> q = new LinkedList<>();
         q.add(10);
@@ -16,5 +29,8 @@ public class Basics {
         int ans = q.element(); // try remove, poll, peek, element
         System.out.println(q);
         System.out.println("ans: " + ans);
+
+        display(q);
+        System.out.println(q);
     }
 }
